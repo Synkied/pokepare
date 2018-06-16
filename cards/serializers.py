423720547ae18e4629180a473414110df80b3252
Synkied <synkx@hotmail.fr>
2018-06-16 11:35:52 +0200

    serializer

--- cards/serializers.py
+++ cards/serializers.py
@@ -3,6 +3,8 @@ from .models import Card
 
 
 class CardSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Card
         fields = '__all__'
