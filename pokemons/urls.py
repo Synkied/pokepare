92e265d9b2b8701c17f3d7a7f8e694751b71a1d7
Synkied <synkx@hotmail.fr>
2018-07-25 17:18:45 +0200

    cache page update

--- pokemons/urls.py
+++ pokemons/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.urls import path
-from django.views.decorators.cache import cache_page
-
 
 from . import views
 
 app_name = "pokemons"
 
 urlpatterns = [
-    path('', cache_page(60 * 60)(views.PokemonView.as_view()), name="pokemon_list"),
-    path('<str:name>', cache_page(60 * 60)(views.PokemonViewDetail.as_view()), name="pokemon_detail"),
+    path('', views.PokemonView.as_view(), name="pokemon_list"),
+    path('<str:name>', views.PokemonViewDetail.as_view(), name="pokemon_detail"),
 ]
