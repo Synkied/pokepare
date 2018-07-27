429cdf7a02360c68c959b545c7e15cbd48e464ec
Synkied <synkx@hotmail.fr>
2018-07-27 16:01:28 +0200

    some rework for travis

--- uploads/tests.py
+++ uploads/tests.py
@@ -24,7 +24,7 @@ class UploadFileTest(TestCase):
         response = myClient.post(reverse('file_upload'), form_data, follow=True)
 
         # get the json response and verify it succeeds.
-        self.assertJSONEqual(response.content, {"success": True, "result": ""})
+        self.assertEqual(response.content, b'{"success": true, "result": ""}')
 
     def test_upload_invalid_file(self):
         """
