c45fd6a39605e046358681d6c0d9bac1d20d3deb
Synkied <synkx@hotmail.fr>
2018-09-19 19:10:57 +0200

    trying a setting for mixed content

--- pokepare/settings/__init__.py
+++ pokepare/settings/__init__.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
